1a26584c6bb56983eae81f0c9a7e656a2bce6071
XIV-XD <82765028+XIV-XD@users.noreply.github.com>
2022-12-23 00:17:07 +0530

    optimized for mobile

--- script.js
+++ script.js
@@ -131,34 +131,5 @@ document.getElementById("navbar").onclick = function (e) {
   }
 }
 
-// NAV BAR
-
-
-// const body = document.querySelector("body"),
-//   nav = document.querySelector("nav"),
- 
- 
-//   sidebarOpen = document.querySelector(".sidebarOpen"),
-//   siderbarClose = document.querySelector(".siderbarClose");
-
-
-// //   js code to toggle sidebar
-// sidebarOpen.addEventListener("click", () => {
-//   nav.classList.add("active");
-// });
-
-// body.addEventListener("click", (e) => {
-//   let clickedElm = e.target;
-
-//   if (
-//     !clickedElm.classList.contains("sidebarOpen") &&
-//     !clickedElm.classList.contains("menu")
-//   ) {
-//     nav.classList.remove("active");
-//   }
-// });
-
-// NAV BAR END
-
 
 
